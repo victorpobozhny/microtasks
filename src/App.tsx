c8b3e3d5c4f79d9317f86392c08ff67115a9b8ef
Victor <pobozhny94@gmail.com>
2024-01-09 12:45:38 +0300

    react-router-dom Routes, Route, Navigate to

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,14 @@
 import React, {useState} from 'react';
 import styles from "./components/Site.module.css";
-
+import {
+    Navigate,
+    Route,
+    Routes
+} from "react-router-dom";
+import {Error404} from "./components/pages/Error404";
+import {PageOne} from "./components/pages/PageOne";
+import {PageTwo} from "./components/pages/PageTwo";
+import {PageThree} from "./components/pages/PageThree";
 
 function App() {
     return (
@@ -11,7 +19,23 @@ function App() {
                     Здесь будет навигация
                 </div>
                 <div className={styles.content}>
-                    Здесь будут кроссовки
+                    <Routes>
+
+                        <Route path="/" element={< Navigate to={'page1'}/>}/>
+
+
+                        <Route path="/page1" element={<PageOne/>}/>
+                        <Route path="/page2" element={<PageTwo/>}/>
+                        <Route path="/page3" element={<PageThree/>}/>
+
+
+
+                        <Route path="/error404" element={<Error404/>}/>
+
+
+                        <Route path='*' element={<Navigate to={'/error404'}/>}/>
+                    </Routes>
+
                 </div>
             </div>
             <div className={styles.footer}>abibas 2023</div>
--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
@@ -9,7 +9,12 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-    <App />
+
+    //HashRouter если используем gh-pages
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+
   );
 
 // If you want to start measuring performance in your app, pass a function
