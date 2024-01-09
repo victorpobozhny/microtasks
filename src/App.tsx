f50bf9329540b0107be87cbb3b4403565fd2b136
Victor <pobozhny94@gmail.com>
2024-01-09 11:31:43 +0300

    react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,96 +1,23 @@
 import React, {useState} from 'react';
-import './App.css';
-import {Todolist} from './Todolist';
-import {v1} from "uuid";
+import styles from "./components/Site.module.css";
 
-
-export type FilterValuesType = "all" | "active" | "completed";
-
-type ToodolistsType = {
-    id: string
-    title: string
-    filter: FilterValuesType
-}
 
 function App() {
-
-    let todolistID1 = v1();
-    let todolistID2 = v1();
-
-    let [todolists, setTodolists] = useState<ToodolistsType[]>([
-        {id: todolistID1, title: 'What to learn', filter: 'all'},
-        {id: todolistID2, title: 'What to buy', filter: 'all'},
-    ])
-
-    let [tasks, setTasks] = useState({
-        [todolistID1]: [
-            {id: v1(), title: "HTML&CSS", isDone: true},
-            {id: v1(), title: "JS", isDone: true},
-            {id: v1(), title: "ReactJS", isDone: false},
-            {id: v1(), title: "Rest API", isDone: false},
-            {id: v1(), title: "GraphQL", isDone: false},
-        ],
-        [todolistID2]: [
-            {id: v1(), title: "HTML&CSS2", isDone: true},
-            {id: v1(), title: "JS2", isDone: true},
-            {id: v1(), title: "ReactJS2", isDone: false},
-            {id: v1(), title: "Rest API2", isDone: false},
-            {id: v1(), title: "GraphQL2", isDone: false},
-        ]
-    });
-
-    function removeTask(todolistID: string, id: string) {
-        setTasks({...tasks, [todolistID]: tasks[todolistID].filter(el => el.id !== id)})
-    }
-
-    function addTask(todolistID: string, title: string) {
-        setTasks({...tasks, [todolistID]: [...tasks[todolistID], {id: v1(), title: title, isDone: false}]})
-    }
-
-
-    function changeStatus(todolistID: string, taskId: string, isDone: boolean) {
-        setTasks({...tasks, [todolistID]: tasks[todolistID].map(el => el.id == taskId ? {...el, isDone: isDone} : el)})
-    }
-
-    function changeFilter(todolistID: string, value: FilterValuesType) {
-        setTodolists(todolists.map(el => el.id == todolistID ? {...el, filter: value} : el))
-    }
-
-    function removeTodolist(todolistID: string) {
-        setTodolists(todolists.filter(el=>el.id!==todolistID))
-        delete(tasks[todolistID])
-    }
-
     return (
-        <div className="App">
-
-            {
-                todolists.map(el => {
-                        let tasksForTodolist = tasks[el.id];
-                        if (el.filter === "active") {
-                            tasksForTodolist = tasksForTodolist.filter(t => !t.isDone);
-                        }
-                        if (el.filter === "completed") {
-                            tasksForTodolist = tasksForTodolist.filter(t => t.isDone);
-                        }
-                        return (
-                            <Todolist key={el.id}
-                                      todolistID={el.id}
-                                      title={el.title}
-                                      tasks={tasksForTodolist}
-                                      removeTask={removeTask}
-                                      changeFilter={changeFilter}
-                                      addTask={addTask}
-                                      changeTaskStatus={changeStatus}
-                                      filter={el.filter}
-                                      removeTodolist={removeTodolist}
-                            />
-                        )
-                    }
-                )
-            }
+        <div>
+            <div className={styles.header}><h1>HEADER</h1></div>
+            <div className={styles.body}>
+                <div className={styles.nav}>
+                    Здесь будет навигация
+                </div>
+                <div className={styles.content}>
+                    Здесь будут кроссовки
+                </div>
+            </div>
+            <div className={styles.footer}>abibas 2023</div>
         </div>
     );
 }
+
 
 export default App;
